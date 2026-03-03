658b669a13d0846a9518631d8b2d787b081013b2
Ere-yiga <erehdagr8@gmail.com>
2026-03-03 21:28:13 +0100

    updated te read me

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Pagy")),
-      ),
-      
+      home: (),
     );
   }
 }
